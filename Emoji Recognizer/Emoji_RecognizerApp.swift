14d796f3cb9dd936ba23761711a8b70a06472eb1
alageev <alageev01@gmail.com>
2020-12-03 22:00:33 +0300

    Initial Commit

---
+++ Emoji Recognizer/Emoji_RecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_RecognizerApp.swift
+//  Emoji Recognizer
+//
+//  Created by Алексей Агеев on 03.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_RecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
